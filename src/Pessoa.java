b2573c1e49364ab336997de257ac996472000f71
Jackson <jacksonaparesido@gmail.com>
2024-03-21 09:52:42 -0300

    first commit

---
+++ src/Pessoa.java
@@ -0,0 +1,86 @@
+public class Pessoa {
+
+    private double peso;
+
+
+    private double altura;
+    private double imc;
+    private String sexo;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getImc() {
+        return imc;
+    }
+
+    public void setImc(double imc) {
+        this.imc = imc;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+
+    public void calculaIMC(){
+
+        this.imc = peso / (Math.pow(altura, 2));
+
+    }
+
+    public  String verificarSaude(){
+        calculaIMC();
+    if (this.sexo.equals("Masculino")){
+        if (this.imc < 20.7 ){
+            return "abaixo do peso";
+        }
+        else if (this.imc < 26.4) {
+            return "no peso normal";
+        }
+        else if (this.imc < 27.8) {
+            return "marginalmente acima do peso";
+        }
+        else if (this.imc < 31.1) {
+            return "acima do peso ideal";
+        }
+        else return "Obeso";
+    }
+    else if (this.sexo.equals("Feminino")) {
+        if (this.imc < 19.1 ){
+            return "abaixo do peso";
+        }
+        else if (this.imc < 25.8) {
+            return "no peso normal";
+        }
+        else if (this.imc < 27.3) {
+            return "marginalmente acima do peso";
+        }
+        else if (this.imc < 32.3) {
+            return "acima do peso ideal";
+        }
+        else return "Obeso";
+    }
+
+
+        return "Sexo invalido";
+    }
+
+}
---
+++ src/PessoaTest.java
@@ -0,0 +1,93 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PessoaTest {
+
+    Pessoa pessoa;
+
+    @BeforeEach
+    void setUp() {
+        pessoa = new Pessoa();
+    }
+
+    @Test
+    void deveRetornarHomemAbaixoPeso() {
+        pessoa.setSexo("Masculino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(20.6f);
+        assertEquals("abaixo do peso", pessoa.verificarSaude());
+    }
+
+    @Test
+    void deveRetornarHomemPesoNormal() {
+        pessoa.setSexo("Masculino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(26.3f);
+        assertEquals("no peso normal", pessoa.verificarSaude());
+    }
+    @Test
+    void deveRetornarHomemAcimaPeso() {
+        pessoa.setSexo("Masculino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(27.7f);
+        assertEquals("marginalmente acima do peso", pessoa.verificarSaude());
+    }
+    @Test
+    void deveRetornarHomemAcimaPesoIdeal() {
+        pessoa.setSexo("Masculino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(31f);
+        assertEquals("acima do peso ideal", pessoa.verificarSaude());
+    }
+    @Test
+    void deveRetornarHomemObeso() {
+        pessoa.setSexo("Masculino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(31.2f);
+        assertEquals("Obeso", pessoa.verificarSaude());
+    }
+    @Test
+    void deveRetornarMulherAbaixoPeso() {
+        pessoa.setSexo("Feminino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(19f);
+        assertEquals("abaixo do peso", pessoa.verificarSaude());
+    }
+    @Test
+    void deveRetornarMulherPesoNormal() {
+        pessoa.setSexo("Feminino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(25.7f);
+        assertEquals("no peso normal", pessoa.verificarSaude());
+    }
+    @Test
+    void deveRetornarMulherAcimaPeso() {
+        pessoa.setSexo("Feminino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(27.2f);
+        assertEquals("marginalmente acima do peso", pessoa.verificarSaude());
+    }
+    @Test
+    void deveRetornarMulherAcimaPesoIdeal() {
+        pessoa.setSexo("Feminino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(32.2f);
+        assertEquals("acima do peso ideal", pessoa.verificarSaude());
+    }
+    @Test
+    void deveRetornarMulherObesa() {
+        pessoa.setSexo("Feminino");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(32.4f);
+        assertEquals("Obeso", pessoa.verificarSaude());
+    }
+    @Test
+    void deveRetornarSexoInvalido() {
+        pessoa.setSexo("Masculin");
+        pessoa.setAltura(1.0f);
+        pessoa.setPeso(32.4f);
+        assertEquals("Sexo invalido", pessoa.verificarSaude());
+    }
+}
